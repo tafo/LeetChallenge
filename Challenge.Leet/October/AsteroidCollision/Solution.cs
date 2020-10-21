adf43e9764f24546b073705c063f2d0db1a22a1f
unknown <tayfunyirdem@gmail.com>
2020-10-21 16:09:56 -0700

    October - AsteroidCollision - Add - Solution

---
+++ Challenge.Leet/October/AsteroidCollision/Solution.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using FluentAssertions;
+
+namespace Challenge.Leet.October.AsteroidCollision
+{
+    /// <summary>
+    /// https://leetcode.com/explore/challenge/card/october-leetcoding-challenge/561/week-3-october-15th-october-21st/3502/
+    /// </summary>
+    public class Solution
+    {
+        public int[] AsteroidCollision(int[] asteroids)
+        {
+            var asteroidStack = new Stack<int>();
+            foreach (var asteroid in asteroids)
+            {
+                if (asteroid < 0)
+                {
+                    if (asteroidStack.Count == 0 || asteroidStack.Peek() < 0)
+                    {
+                        asteroidStack.Push(asteroid);
+                    }
+                    else
+                    {
+                        var pushedAsteroid = 0;
+                        var absoluteAsteroid = Math.Abs(asteroid);
+                        while (asteroidStack.Count > 0 && asteroidStack.Peek() > 0)
+                        {
+                            pushedAsteroid = asteroidStack.Pop();
+                            if (pushedAsteroid < absoluteAsteroid)
+                            {
+                                continue;
+                            }
+
+                            if (pushedAsteroid == absoluteAsteroid)
+                            {
+                                break;
+                            }
+                            asteroidStack.Push(pushedAsteroid);
+                            break;
+                        }
+
+                        if (pushedAsteroid < absoluteAsteroid)
+                        {
+                            asteroidStack.Push(asteroid);
+                        }
+                    }
+                }
+                else
+                {
+                    asteroidStack.Push(asteroid);
+                }
+            }
+
+            return asteroidStack.Reverse().ToArray();
+        }
+    }
+}
---
+++ Challenge.Leet/October/AsteroidCollision/Test.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Challenge.Leet.October.AsteroidCollision
+{
+    public class Test
+    {
+        private readonly ITestOutputHelper _outputHelper;
+        public Test(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
+
+        [Theory]
+        [MemberData(nameof(InputAndOutput))]
+        public void Check(int[] asteroids, int[] expectedOutput)
+        {
+            var timer = Stopwatch.StartNew();
+            var solution = new Solution();
+            timer.Start();
+            var actualOutput = solution.AsteroidCollision(asteroids);
+            timer.Stop();
+            actualOutput.Should().BeEqualTo(expectedOutput);
+            _outputHelper.WriteLine($"Duration = {timer.ElapsedTicks}");
+        }
+
+        public static IEnumerable<object[]> InputAndOutput => new List<object[]>
+        {
+            new object[] {new[] {5, 10, -5}, new[] {5, 10}},
+            new object[] {new[] {8, -8}, new int[] { }},
+            new object[] {new[] {10, 2, -5}, new[] {10}},
+            new object[] {new[] {-2, -1, 1, 2}, new[] {-2, -1, 1, 2}},
+            new object[] {new[] {-2, -2, 1, -2}, new[] {-2, -2, -2}},
+            new object[] {new[] {-2,1,1,-1}, new[] {-2,1}}
+        };
+    }
+}
