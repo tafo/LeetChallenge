a9377d9b1db6b7fa0537965d4b886cf7f1668fd5
Tayfun Yirdem <tayfunyirdem@gmail.com>
2021-01-24 20:26:36 +0300

    TwentyOne - January - SortTheMatrixDiagonally - Add - Solution

---
+++ Challenge.Leet/TwentyOne/SortTheMatrixDiagonally/Solution.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Challenge.Leet.TwentyOne.SortTheMatrixDiagonally
+{
+    public class Solution
+    {
+        public int[][] DiagonalSort(int[][] mat)
+        {
+            var rowCount = mat.Length;
+            var columnCount = mat[0].Length;
+            for (var i = 0; i < rowCount; i++)
+            {
+                Sort(i, 0);
+            }
+
+            for (var j = 0; j < columnCount; j++)
+            {
+                Sort(0, j);
+            }
+            
+            return mat;
+            
+            void Sort(int i, int j)
+            {
+                var diagonal = new List<int>();
+                
+                while (i < rowCount && j < columnCount)
+                {
+                    diagonal.Add(mat[i][j]);
+                    i++;
+                    j++;
+                }
+                
+                diagonal.Sort();
+                
+                while (i > 0 && j > 0)
+                {
+                    i--;
+                    j--;
+                    mat[i][j] = diagonal.Last();
+                    diagonal.RemoveAt(diagonal.Count - 1);
+                }
+            }
+        }
+    }
+}
---
+++ Challenge.Leet/TwentyOne/SortTheMatrixDiagonally/Test.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using FluentAssertions;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Challenge.Leet.TwentyOne.SortTheMatrixDiagonally
+{
+    public class Test
+    {
+        private readonly ITestOutputHelper _outputHelper;
+
+        public Test(ITestOutputHelper outputHelper)
+        {
+            _outputHelper = outputHelper;
+        }
+
+        [Theory]
+        [MemberData(nameof(InputAndOutput))]
+        public void Check(int[][] matrix, int[][] expectedOutput)
+        {
+            var timer = Stopwatch.StartNew();
+            var solution = new Solution();
+            timer.Start();
+            var actualOutput = solution.DiagonalSort(matrix);
+            timer.Stop();
+            actualOutput.Should().BeEqualTo(expectedOutput);
+            _outputHelper.WriteLine($"Duration = {timer.ElapsedTicks}");
+        }
+
+        public static IEnumerable<object[]> InputAndOutput => new List<object[]>
+        {
+            new object[]
+            {
+                new[]
+                {
+                    new[] {3, 3, 1, 1},
+                    new[] {2, 2, 1, 2},
+                    new[] {1, 1, 1, 2}
+                },
+                new[]
+                {
+                    new[] {1, 1, 1, 1},
+                    new[] {1, 2, 2, 2},
+                    new[] {1, 2, 3, 3}
+                }
+            }
+        };
+    }
+}
